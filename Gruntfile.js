7f4c6c27dd1c01c6b3ee2621a42ecae05d92cde1
zhangzhuozhang <2028251635@qq.com>
2020-02-26 16:53:12 +0800

    eslint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,15 @@
+/* global module: true */
 module.exports = function (grunt) {
 
   grunt.initConfig({
-
-    csslint: {
-
+    eslint: {
       options: {
-
-        csslintrc: '.csslintrc'
-
+        configFile: '.eslintrc.json'
       },
-      src: ['*.css']
+      target: ['*.js']
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-csslint');
-  grunt.registerTask('default', ['csslint']);
-
+  grunt.loadNpmTasks('grunt-eslint');
+  grunt.registerTask('default', ['eslint']);
 };
--- rectangle.js
+++ rectangle.js
@@ -1,13 +1,15 @@
+/* global $: true */
+
 $(function(){
   var $width=$('#width'),
       $height=$('#height'),
       $btnCal = $('#calculate'),
       $perimeter=$('#perimeter'),
       $area=$('#area');
-$btnCal.click(function(){
+  $btnCal.click(function(){
   var w=Number($width.val()),
-     h=Number($height.val());
-  var p=Math.round(2*(w+h)*Math.pow(10,4))/Math.pow(10,4);
+      h=Number($height.val());
+  var p=Math.round(2*(w+h)*Math.pow(10,4))/Math.pow(10,4),
       a=Math.round(w*h*Math.pow(10,4))/Math.pow(10,4);
       $perimeter.val(p);
       $area.val(a);
