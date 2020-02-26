b5dbbe75b7469fb1879c727c9a6afc382e7a4cbe
zhangzhuozhang <2028251635@qq.com>
2020-02-26 09:12:23 +0800

    html

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,28 @@
-module.exports = function(grunt){
+
+module.exports = function (grunt) {
+
   grunt.initConfig({
-    less:{
-      compile:{
-        files:{'dist/compiled.css':'css/*.less'}
-      }
+
+    htmlhint: {
+
+      options: {
+
+        htmlhintrc: '.htmlhintrc'
+
+      },
+
+      src: ['*.html']
+
     }
+
   });
 
-  grunt.loadNpmTasks('grunt-contrib-less');
 
-  grunt.registerTask('default','less:compile');
-}
+
+  grunt.loadNpmTasks('grunt-htmlhint');
+
+
+
+  grunt.registerTask('default', ['htmlhint']);
+
+};
